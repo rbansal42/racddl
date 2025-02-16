58c0c17c5e8daa97b0cbbe170fcfa3fdfc259e69
Kanan Mittal <kananmittal05122005@gmail.com>
2025-02-16 13:25:49 +0530

    Core team added

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -7,16 +7,39 @@ const Team = () => {
       {
         name: "Rtr. Rajat Kapoor",
         position: "Immediate Past President",
-        image: "1.png",
+        image: "rajat.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       },
       {
         name: "Ayush Aggarwal",
         position: "Club President",
-        image: "2.png",
+        image: "ayush.png",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       },
     ];
+
+    const coreMembers = [
+        {
+          name: "Rahul Bansal",
+          position: "Secretary",
+          image: "/rahul.png", // Dummy image
+        },
+        {
+          name: "Maanya Jain",
+          position: "Vice President",
+          image: "/maanya.png", // Dummy image
+        },
+        {
+          name: "Kanan Mittal",
+          position: "Joint Secretary",
+          image: "/kanan.png", // Dummy image
+        },
+        {
+          name: "Chirangini Oberoi",
+          position: "Cheif-Aid to President",
+          image: "/maanya.png", // Dummy image
+        },
+      ];
   
     return (
       <div className={styles.teamContainer}>
@@ -33,6 +56,17 @@ const Team = () => {
             </div>
           ))}
         </div>
+        <div className={styles.coreTeamGrid}>
+        {coreMembers.map((coreMember, index) => (
+          <div className={styles.coreTeamMember} key={index}>
+            <div className={styles.squareContainer}>
+              <h2 className={styles.coreMemberName}>{coreMember.name}</h2>
+              <h3 className={styles.coreMemberPosition}>{coreMember.position}</h3>
+            </div>
+            <img src={coreMember.image} alt={coreMember.name} className={styles.coreMemberImage} />
+          </div>
+        ))}
+      </div>
       </div>
     );
   };
